c9b4da7b11ddaa0876b380974370cff9d87de0d4
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-18 21:15:31 -0300

    Validação de usuário e senha

---
+++ src/main/java/keysson/apis/validacao/exception/enums/ErrorCode.java
@@ -0,0 +1,25 @@
+package keysson.apis.validacao.exception.enums;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+        USER_NOT_FOUND("Usuário não existe.", HttpStatus.BAD_REQUEST),
+        BAD_PASSWORD("Senha incorreta", HttpStatus.BAD_REQUEST)
+    ;
+
+    private final String message;
+    private final HttpStatus status;
+
+    ErrorCode(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
