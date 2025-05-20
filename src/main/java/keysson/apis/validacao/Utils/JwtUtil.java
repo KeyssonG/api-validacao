5c189e285a80d9710219d670f38b5f3a892c4280
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-20 20:07:51 -0300

    Correcao jwtUtil

--- src/main/java/keysson/apis/validacao/Utils/JwtUtil.java
+++ src/main/java/keysson/apis/validacao/Utils/JwtUtil.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class JwtUtil {
 
     @Value("${SECRET_KEY}")
-    private final String SECRET_KEY;
+    private String SECRET_KEY;
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
