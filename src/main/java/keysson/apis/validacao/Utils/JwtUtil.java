a89488ee310d0a35cbb2e74f33e028976188806e
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-20 11:05:02 -0300

    Criacao de arquivos para pipelin

--- src/main/java/keysson/apis/validacao/Utils/JwtUtil.java
+++ src/main/java/keysson/apis/validacao/Utils/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class JwtUtil {
 
-    private final String SECRET_KEY = "0MEI6P/upC+MtYqAiki5pV0Zwpnm8zo12OLnLK6SQWI=";
+    private final String SECRET_KEY = "SECRET_KEY";
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
