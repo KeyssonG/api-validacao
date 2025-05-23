491587580a5990124c3def45104c5d8e9d8568d2
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-23 01:25:13 -0300

    fix: cors

--- src/main/java/keysson/apis/validacao/config/SecurityConfig.java
+++ src/main/java/keysson/apis/validacao/config/SecurityConfig.java
@@ -27,9 +27,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/v3/api-docs",
                                 "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/configuration/**",
                                 "/webjars/**"
                         ).permitAll()
                         .requestMatchers("/login").permitAll()
